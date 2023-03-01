2c929b07bb3d0dd1049d7af4ad7cb1506bb6650f
Ankraid <77840124+Intov@users.noreply.github.com>
2023-03-01 17:47:42 +0300

    Add files via upload

---
+++ calc/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        calculateTest.delTest();
+        calculateTest.multypleTest();
+        calculateTest.summTest();
+        calculateTest.vozvTest();
+        calculateTest.vychTest();
+        System.out.println(calculate.vych(4,2));
+        System.out.println(calculate.del(2,2));
+    }
+}
